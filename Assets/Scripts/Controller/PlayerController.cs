1d3e243f4c338d55fe917aeca20f1431bad709aa
JAZWii <jazwii2216@gmail.com>
2021-06-22 23:58:25 +0300

    First Commit

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// This class handles the movement of the player with given input from the input manager
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("The speed at which the player moves")]
+    public float moveSpeed = 2f;
+    [Tooltip("The speed at which the player rotates to look left and right (calculated in degrees)")]
+    public float lookSpeed = 60f;
+    [Tooltip("The power with which the player jumps")]
+    public float jumpPower = 8f;
+    [Tooltip("The strength of gravity")]
+    public float gravity = 9.81f;
+    [Header("Required References")]
+    [Tooltip("The player shooter script that fires projectiles")]
+    public Shooter playerShooter;
+
+    //The character controller component on the player
+    private CharacterController controller;
+    private InputManager inputManager;
+    
+    /// <summary>
+    /// Description:
+    /// Standard Unity function called once before the first Update call
+    /// Input:
+    /// none
+    /// Return:
+    /// void (no return)
+    /// </summary>
+    void Start()
+    {
+        SetupCharacterController();
+        SetUpInputManager();
+    }
+
+    private void SetupCharacterController()
+    {
+        controller = GetComponent<CharacterController>();
+        if (controller == null)
+        {
+            Debug.LogError("The player controller script does not have a character controller on the same game object!");
+        }
+    }
+
+    void SetUpInputManager()
+    {
+        inputManager = InputManager.instance;
+    }
+    
+    /// <summary>
+    /// Description:
+    /// Standard Unity function called once every frame
+    /// Input:
+    /// none
+    /// Return:
+    /// void (no return)
+    /// </summary>
+    void Update()
+    {
+        ProcessMovement();
+    }
+
+    private Vector3 moveDirection;
+    void ProcessMovement()
+    {
+        //Get the input from the input manager
+        float leftRightInput = inputManager.horizontalMoveAxis;
+        float forwardBackwardInput = inputManager.verticalMoveAxis;
+        bool jumpPressed = inputManager.jumpPressed;
+        
+        //
+        if (controller.isGrounded)
+        {
+            // set the movement direction to be the received input, set y to 0 since we are on the ground
+            moveDirection = new Vector3(leftRightInput, 0, forwardBackwardInput);
+            // set the move direction in relation to the transform
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * moveSpeed;
+
+            if (jumpPressed)
+            {
+                moveDirection.y = jumpPower;
+            }
+        }
+        else
+        {
+            moveDirection = new Vector3(leftRightInput * moveSpeed, moveDirection.y, forwardBackwardInput * moveSpeed);
+            moveDirection = transform.TransformDirection(moveDirection);
+
+        }
+        
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+    
+}
